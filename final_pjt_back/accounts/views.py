c7b51dbbb56eac3b29db09d0a651a4d750ffe8be
Changjae-YOO <dondegi5@gmail.com>
2023-05-18 12:50:02 +0900

    feat: accounts 기능 추가

--- final_pjt_back/accounts/views.py
+++ final_pjt_back/accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 # Create your views here.
+# def login(request):
+#     token = Token.objects.create(u)
