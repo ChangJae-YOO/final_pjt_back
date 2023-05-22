2c73f65dc718509914b9ee643dc30537ddb91185
Changjae-YOO <dondegi5@gmail.com>
2023-05-22 09:58:04 +0900

    fix: comment like 구현을 위해 수정

--- final_pjt_back/movies/serializers.py
+++ final_pjt_back/movies/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Movie, Genre, Comment
+from django.contrib.auth import get_user_model
+
 
 
 class GenreSerializer(serializers.ModelSerializer):
@@ -8,13 +10,22 @@ class GenreSerializer(serializers.ModelSerializer):
         model = Genre
         fields = ('name',)
 
+
 class CommentSerializer(serializers.ModelSerializer):
     user_name = serializers.CharField(source = 'user.username', read_only = True)
 
     class Meta:
         model = Comment
-        fields = ('content', 'created_at', 'updated_at', 'user_name', 'id')
+        fields = '__all__'
         read_only_fields = ('movie',)
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['comment_user'] = []
+        User = get_user_model()
+        for user_id in rep['comment_likes']:
+            rep['comment_user'].append(User.objects.get(pk=user_id).username)
+        return rep
 
 class MovieSerailizer(serializers.ModelSerializer):
     genres = GenreSerializer(many=True, read_only=True)
